53d0ccf010d4f54c06a4d71489567b15d2c06543
Yeji <101444425+mnbvcxzyj@users.noreply.github.com>
2022-03-14 20:03:35 +0900

    Create Challenge4_1.c

---
+++ 김예지/Challenge4_1.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int gcd(int x, int y);
+int main(void)
+{
+	int a, b, big, small;
+
+	printf("Enter two numbers : ");
+	scanf("%d %d", &a, &b);
+
+	if (a>b)
+	{
+		big = b;
+		small = a;
+	}
+	else
+	{
+		big = a;
+		small = b;
+	}
+
+	printf("%d와 %d의 최대공약수는 %d\n", a, b, gcd(big, small));
+}
+
+int gcd(int x, int y)
+{
+	int z;
+
+	while (y) {
+		z = x % y;
+		x = y;
+		y = z;
+	}
+	return x;
+}
